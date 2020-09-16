dbda1c1b867911ab140cd2b23a9a3b69ef8ed2c0
beriny <beriny_sh@163.com>
2020-09-16 13:44:06 +0800

    update

---
+++ Canvas/App/compiler/src/Monkey.js
@@ -0,0 +1,30 @@
+import React , { Component } from 'react';
+import * as bootstrap from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+class Monkey extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render () {
+        let textAreaStyle = {
+            height: 480
+        };
+        return (
+            <bootstrap.Panel header="Monkey Compiler" bsStyle="success">
+                <bootstrap.FormGroup>
+                    <bootstrap.FormControl componentClass = "textarea"
+                        style={textAreaStyle}
+                        placeholder="Enter your code" />
+                </bootstrap.FormGroup>
+                <bootstrap.Button bsStyle="danger">
+                  Lexing
+                </bootstrap.Button>
+            </bootstrap.Panel>
+        );
+    }
+}
+
+export default Monkey;
