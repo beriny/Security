1b5273e3ccd9553d0a0b647c8d6044099e204bb2
beriny <beriny_sh@163.com>
2020-09-23 21:25:43 +0800

    change the complier config

--- Canvas/App/complier/src/index.js
+++ Canvas/App/complier/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Monkey from './Monkey';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Monkey />,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
