4d69b883b7e1996ad6cd7742e9c41ae496ce92c1
Bering0er <sinkdwty@outlook.com>
2020-11-08 11:32:21 +0800

    add cheerio to query the website

--- Canvas/App/htmlWeb/views.js
+++ Canvas/App/htmlWeb/views.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const cheerio = require('cheerio')
-const request = require('request')
+const express = require('express');
+const cheerio = require('cheerio');
+const request = require('request');
+const superagent = require('superagent');
 
-
-const app = express()
+const app = express();
 
 
 app.get('/', function(req, res) {
@@ -14,10 +14,32 @@ app.get('/', function(req, res) {
             res.json({
                 'status': 200,
                 'id': $("#btop").length
-            })
-        }
-    })
-})
+            });
+        };
+    });
+});
 
+// callback
+superagent
+  .post('/api/pet')
+  .send({ name: 'Manny', species: 'cat' }) // sends a JSON post body
+  .set('X-API-Key', 'foobar')
+  .set('accept', 'json')
+  .end((err, res) => {
+    // Calling the end function will send the request
+  });
+
+// promise with then/catch
+superagent.post('/api/pet').then(console.log).catch(console.error);
+
+// promise with async/await
+(async () => {
+  try {
+    const res = await superagent.post('/api/pet');
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+})();
 
 app.listen(3001)
